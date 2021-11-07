9159d714b5d7bc6caeba8db59cedebb9713a67b4
Nilesh9492 <nilesh94041@gmail.com>
2021-11-07 12:50:19 +0530

    Creating Addressbook Table

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -2,4 +2,15 @@
 create database AddressBook;
 use AddressBook;
 
-
+--UC2--
+create table Address_Book_Table
+(
+FirstName varchar(50) not null,
+LastName varchar(20),
+Address varchar(100),
+City varchar(50),
+StateName varchar(55),
+ZipCode int,
+Phonenum bigint,
+EmailId varchar(100)
+);
